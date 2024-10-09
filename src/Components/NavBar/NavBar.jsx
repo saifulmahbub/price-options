34a19ec9ae1476e8af6591c7f16c9e975d4200e5
saifulmahbub <saifulislammahbub83@gmail.com>
2024-10-09 19:34:57 +0200

    toggle down the menu bar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -13,12 +13,16 @@ const NavBar = () => {
   ];
   const [open, setOpen] = useState(false);
   return (
-    <nav>
+    <nav className="p-6 bg-yellow-200">
       <div className="md:hidden text-2xl" onClick={() => setOpen(!open)}>
         {open === true ? <AiFillDelete /> : <RiMenu2Line />}
       </div>
 
-      <ul className="md:flex ">
+      <ul
+        className={`md:flex absolute duration-1000 md:static ${
+          open ? "top-16" : "-top-60"
+        } bg-yellow-200 px-6 `}
+      >
         {routes.map((route) => (
           <Link key={route.id} route={route}></Link>
         ))}
