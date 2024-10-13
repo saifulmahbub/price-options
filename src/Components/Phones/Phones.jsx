101301596d3d68efcaa4a2b7821c6adf73edeef8
saifulmahbub <saifulislammahbub83@gmail.com>
2024-10-13 22:45:23 +0200

    added barchart and others

--- src/Components/Phones/Phones.jsx
+++ src/Components/Phones/Phones.jsx
@@ -1,7 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Audio } from "react-loader-spinner";
+
+import {
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
+
 const Phones = () => {
+  const [phones, setPhones] = useState([]);
+  useEffect(() => {
+    fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
+      .then((res) => res.json())
+      .then((data) => setPhones(data.data));
+    //   axios
+    //     .get("https://openapi.programming-hero.com/api/phones?search=iphone")
+    //     .then((data) => setPhones(data.data.data));
+  }, []);
   return (
     <div>
-      <h1>saiful islam</h1>
+      <Audio
+        height="80"
+        width="80"
+        radius="9"
+        color="green"
+        ariaLabel="three-dots-loading"
+        wrapperStyle
+        wrapperClass
+      />
+      <h1>phones:{phones.length}</h1>
     </div>
   );
 };
